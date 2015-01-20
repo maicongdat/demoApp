627efdd6e4365003863a9e10adb2eaab453a6285
maicongdat <mai.cong.dat@framgia.com>
2015-01-20 15:51:57 +0700

    remove activate email

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,9 @@ class User < ActiveRecord::Base
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
-  attr_accessor :remember_token, :activation_token, :reset_token
-  before_save   :downcase_email
-  before_create :create_activation_digest
+  # attr_accessor :remember_token, :activation_token, :reset_token
+  # before_save   :downcase_email
+  # before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -42,7 +42,7 @@ class User < ActiveRecord::Base
 
   # Returns true if the given token matches the digest.
   def authenticated?(attribute, token)
-    digest = send("#{attribute}_digest")
+    # digest = send("#{attribute}_digest")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
@@ -96,13 +96,12 @@ class User < ActiveRecord::Base
 
     # Converts email to all lower-case.
     def downcase_email
-      self.email = email.downcase
+      
     end
 
     # Creates and assigns the activation token and digest.
     def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
+      
     end
     
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,9 @@ User.create!(name:  "Example User",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true,
-             activated: true,
-             activated_at: Time.zone.now)
+             # activated: true,
+             # activated_at: Time.zone.now
+             )
 
 99.times do |n|
   name  = Faker::Name.name
@@ -14,8 +15,9 @@ User.create!(name:  "Example User",
                email: email,
                password:              password,
                password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
+               # activated: true,
+               # activated_at: Time.zone.now
+               )
 end
 users = User.order(:created_at).take(6)
 50.times do
